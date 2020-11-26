d0f244a99f31b1ab9b0a9dd44a48fe9d4c5953fc
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-11-26 11:13:02 +0100

    Babel polyfill installed

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env) => {
     const isProduction = env && env.production
     return {
         mode: isProduction ? 'production' : 'development',
-        entry: './src/app.js',
+        entry: ['@babel/polyfill', './src/app.js'],
         output: {
             path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
