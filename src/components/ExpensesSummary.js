5da0d3368f74b0dce6093ec303833e21d5fcb4e7
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-11-29 05:52:44 +0100

    Added hidden expenses message

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom'
 import getExpensesTotal from '../selectors/expenses-total';
 import getVisibleExpenses from '../selectors/expenses';
 
-export const ExpensesSummary = ({ totalAmount, expenseCount }) => {
-    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
-    const beWord = expenseCount === 1 ? 'is' : 'are';
+export const ExpensesSummary = ({ totalAmount, expensesCount, allExpensesCount }) => {
+    const expenseWord = expensesCount === 1 ? 'expense' : 'expenses';
+    const beWord = expensesCount === 1 ? 'is' : 'are';
+    const allExpenseWord = allExpensesCount === 1 ? 'expense' : 'expenses';
+    const hidenExpensesCount = allExpensesCount - expensesCount;
     return <div className="page-header">
         <div className="content-container">
-            <h2 className="page-header__title">There {beWord} <span>{expenseCount}</span> {expenseWord} totalling to <span>{totalAmount}</span></h2>
+            <h2 className="page-header__title">There {beWord} <span>{expensesCount}</span> {expenseWord} totalling to <span>{totalAmount}</span></h2>
+            {!!hidenExpensesCount && <p>Not showing {hidenExpensesCount} out of {allExpensesCount} {allExpenseWord} because of filters</p>}
             <div>
                 <Link to="/create" className="button">Create</Link>
             </div>
@@ -21,7 +24,8 @@ const mapStateToProps = (state) => {
     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
     return {
         totalAmount: getExpensesTotal(visibleExpenses),
-        expenseCount: visibleExpenses.length
+        expensesCount: visibleExpenses.length,
+        allExpensesCount: state.expenses.length
     }
 }
 
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -2,12 +2,17 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {ExpensesSummary} from '../../components/ExpensesSummary';
 
-test('Should correctly render ExpensesSummary for multiple expenses', () => {
-    const wrapper = shallow(<ExpensesSummary totalAmount={1200} expenseCount={3} />);
+test('Should correctly render ExpensesSummary for multiple expenses out of many', () => {
+    const wrapper = shallow(<ExpensesSummary totalAmount={1200} expensesCount={3} allExpensesCount={6} />);
     expect(wrapper).toMatchSnapshot();
 });
 
-test('Should correctly render ExpensesSummary for multiple expenses', () => {
-    const wrapper = shallow(<ExpensesSummary totalAmount={1200} expenseCount={1} />);
+test('Should correctly render ExpensesSummary for single expense out of many', () => {
+    const wrapper = shallow(<ExpensesSummary totalAmount={1200} expensesCount={1} allExpensesCount={6} />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should correctly render ExpensesSummary for single expense out of one', () => {
+    const wrapper = shallow(<ExpensesSummary totalAmount={1200} expensesCount={1} allExpensesCount={1} />);
     expect(wrapper).toMatchSnapshot();
 });
